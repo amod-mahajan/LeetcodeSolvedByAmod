fde945bb4a5546e646f3ede17071de010d7c8eec
amod.mahajan <amod.mahajan@hotmail.com>
2023-04-30 18:18:41 -0400

    JewelsAndStones explanations

--- src/main/java/strings/easy/JewelsAndStones.java
+++ src/main/java/strings/easy/JewelsAndStones.java
@@ -1,19 +1,22 @@
 package strings.easy;
 
-import java.util.Calendar;
 import java.util.HashMap;
-
+// https://leetcode.com/problems/jewels-and-stones/
 public class JewelsAndStones {
 
     public static int numJewelsInStones(String jewels, String stones) {
-        int count = 0;
-        HashMap<Character, Integer> charCounts = new HashMap();
+        // A map to store count of each jewels in stones i.e. store occurrence of each char in stones
+        HashMap<Character, Integer> charCounts = new HashMap<>();
+        // Iterate char by char and store in map
         for(char c : stones.toCharArray())
             charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
-
+        // Variable to count jewels in stones
+        int count = 0;
+        // Iterate char by char in jewels and get the value from map
         for(char c : jewels.toCharArray())
+            // add values for each char
             count +=  charCounts.getOrDefault(c, 0);
-
+        // return count
         return count;
     }
 
